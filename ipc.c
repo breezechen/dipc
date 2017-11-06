31758c3a31870e5a2143f0b44d9d085037f4e71a
Breeze Chen <breezechen356@gmail.com>
2017-11-06 21:01:36 +0800

    Fix: when create "Global\*" events, should setting the appropriate security descriptor at object creation time.

--- ipc.c
+++ ipc.c
@@ -1,6 +1,44 @@
 #include <windows.h>
 #include "ipc.h"
 
+BOOL SetPrivilege(LPCTSTR lpszPrivilege, BOOL bEnablePrivilege)
+{
+	TOKEN_PRIVILEGES tp;
+	LUID luid;
+	HANDLE hToken;
+	BOOL ret = FALSE;
+
+	do
+	{
+		if (!OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES, &hToken)) {
+			break;
+		}
+
+		if (!LookupPrivilegeValue(NULL, lpszPrivilege, &luid)) {
+			break;
+		}
+
+		tp.PrivilegeCount = 1;
+		tp.Privileges[0].Luid = luid;
+		if (bEnablePrivilege)
+			tp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+		else
+			tp.Privileges[0].Attributes = 0;
+
+		if (!AdjustTokenPrivileges(hToken, FALSE, &tp, sizeof(TOKEN_PRIVILEGES), (PTOKEN_PRIVILEGES)NULL, (PDWORD)NULL)) {
+			break;
+		}
+
+		if (GetLastError() == ERROR_NOT_ALL_ASSIGNED) {
+			break;
+		}
+
+		ret = TRUE;
+	} while (0);
+
+	return ret;
+}
+
 IpcServer* ServerCreate(LPCTSTR serverName)
 {
     IpcServer *ret = NULL;
@@ -15,14 +53,28 @@ IpcServer* ServerCreate(LPCTSTR serverName)
 	TCHAR repeName[MAX_NAME_LEN + 5];
 	TCHAR readyeName[MAX_NAME_LEN + 5];
 	TCHAR doneeName[MAX_NAME_LEN + 5];
+	SECURITY_ATTRIBUTES sa;
+	SECURITY_DESCRIPTOR sd;
 
     do 
     {
+		InitializeSecurityDescriptor(&sd, SECURITY_DESCRIPTOR_REVISION);
+		SetSecurityDescriptorDacl(&sd, TRUE, NULL, FALSE);
+
+		sa.bInheritHandle = FALSE;
+		sa.nLength = sizeof(SECURITY_ATTRIBUTES);
+		sa.lpSecurityDescriptor = &sd;
+
 		if (serverName == NULL || serverName[0] == '\0')
 			serverName = TEXT("simple_icp_default");
 
 		if (lstrlen(serverName) > MAX_NAME_LEN)
 			break;
+
+		if (_strnicmp(serverName, "Global\\", strlen("Global\\")) == 0 && 
+			!SetPrivilege("SeCreateGlobalPrivilege", TRUE)) {
+			break;
+		}
 
 		wsprintf(memName, TEXT("%s_mem"), serverName);
 		wsprintf(reqeName, TEXT("%s_req"), serverName);
@@ -30,31 +82,31 @@ IpcServer* ServerCreate(LPCTSTR serverName)
 		wsprintf(readyeName, TEXT("%s_red"), serverName);
 		wsprintf(doneeName, TEXT("%s_don"), serverName);
 
-		doneEvent = CreateEvent(NULL, FALSE, FALSE, doneeName);
+		doneEvent = CreateEvent(&sa, FALSE, FALSE, doneeName);
 		if (NULL == doneEvent)
 			break;
 		if (GetLastError() == ERROR_ALREADY_EXISTS)
 			break;
 
-        repEvent = CreateEvent(NULL, FALSE, FALSE, repeName);
+        repEvent = CreateEvent(&sa, FALSE, FALSE, repeName);
         if (NULL == repEvent)
             break;
         if (GetLastError() == ERROR_ALREADY_EXISTS)
             break;
 
-        reqEvent = CreateEvent(NULL, FALSE, FALSE, reqeName);
+        reqEvent = CreateEvent(&sa, FALSE, FALSE, reqeName);
         if (NULL == reqEvent)
             break;
         if (GetLastError() == ERROR_ALREADY_EXISTS)
             break;
 
-		readyEvent = CreateEvent(NULL, FALSE, FALSE, readyeName);
+		readyEvent = CreateEvent(&sa, FALSE, FALSE, readyeName);
 		if (NULL == readyEvent)
 			break;
 		if (GetLastError() == ERROR_ALREADY_EXISTS)
 			break;
 
-        mapFile = CreateFileMapping(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, 0, MEMMAP_SIZE, memName);
+        mapFile = CreateFileMapping(INVALID_HANDLE_VALUE, &sa, PAGE_READWRITE, 0, MEMMAP_SIZE, memName);
         if (NULL == mapFile) 
             break;
         if (GetLastError() == ERROR_ALREADY_EXISTS)
@@ -161,11 +213,12 @@ CommPacket* ClientRequest(ULONG cmd, const BYTE* data, SIZE_T size, LPCTSTR serv
 		wsprintf(readyeName, TEXT("%s_red"), serverName);
 		wsprintf(doneeName, TEXT("%s_don"), serverName);
 
-        mapFile = OpenFileMapping(FILE_MAP_ALL_ACCESS, FALSE, memName);
-        if (NULL == mapFile)
-            break;
-
-        buf = (BYTE *)MapViewOfFile(mapFile, FILE_MAP_ALL_ACCESS, 0, 0, MEMMAP_SIZE);
+        mapFile = OpenFileMapping(FILE_MAP_WRITE | FILE_MAP_READ, FALSE, memName);
+		if (NULL == mapFile) {
+			break;
+		}
+            
+        buf = (BYTE *)MapViewOfFile(mapFile, FILE_MAP_WRITE | FILE_MAP_READ, 0, 0, MEMMAP_SIZE);
         if (NULL == buf)
             break;
 
